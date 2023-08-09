6805d27fdca7aad3064e779a1fbe27ff20fb6edf
Md Delwar Hoosain <delwar0cse@gmail.com>
2023-08-09 18:58:30 +0200

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:START_ANYDESK
+anydesk --start
+ping 127.0.0.1 -n 4 > nul
+for /f "delims=" %%i in ('"C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe" --get-id') do (
+    set ID=%%i
+)
+if "%ID%"=="" (
+    taskkill /f /im anydesk.exe > nul
+    goto START_ANYDESK
+)
+if "%ID%"=="0" (
+    taskkill /f /im anydesk.exe > nul
+    goto START_ANYDESK
+)
+
+echo disalardp | anydesk.exe --set-password _full_access
+echo AnyDesk ID is: %ID%
+echo AnyDesk Password: disalardp
+echo You can login now!
