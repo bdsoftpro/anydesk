6176c2e240ce459f0eaf5d8fc326c8163c848050
Md Delwar Hoosain <delwar0cse@gmail.com>
2024-09-16 01:31:22 +0600

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -5,3 +5,4 @@ set password=del!@12sha
 powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 tzutil /s "Central America Standard Time"
+powershell -Command "Invoke-WebRequest https://dl.anyviewer.com/noinstall/AnyViewer.exe -OutFile AnyViewer.exe"
