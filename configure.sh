ad05aac0470830483fcd28600205f58138683337
Md Delwar Hoosain <delwar0cse@gmail.com>
2024-10-03 14:10:49 +0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,4 +6,15 @@ sudo mdutil -i off -a
 # Assign the value random password to the password variable
 rustdesk_pw=$1
 
-echo "Password: $rustdesk_pw"
+#Create new account
+sudo dscl . -create /Users/admin
+sudo dscl . -create /Users/admin UserShell /bin/bash
+sudo dscl . -create /Users/admin RealName "Admin"
+sudo dscl . -create /Users/admin UniqueID 1001
+sudo dscl . -create /Users/admin PrimaryGroupID 80
+sudo dscl . -create /Users/adminr NFSHomeDirectory /Users/admin
+sudo dscl . -passwd /Users/admin $1
+sudo dscl . -passwd /Users/admin $1
+sudo createhomedir -c -u admin > /dev/null
+
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -users admin -access -on -privs -all
